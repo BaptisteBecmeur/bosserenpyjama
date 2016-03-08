0cda9984f1aa8481d50cf66e10dae8c8c9fca32f
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-08 14:23:32 +0100

    Category model migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301222531) do
+ActiveRecord::Schema.define(version: 20160308132249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "post_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "categories", ["post_id"], name: "index_categories_on_post_id", using: :btree
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
@@ -65,5 +75,6 @@ ActiveRecord::Schema.define(version: 20160301222531) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "categories", "posts"
   add_foreign_key "posts", "users"
 end
