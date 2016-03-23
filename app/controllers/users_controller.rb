a3ab5b13ecfea679da0dd05757fbc2e83849181f
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-23 14:50:28 +0100

    views in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate_user!, except: [:index]
+  #before_action :find_user, only: [:show]
 
   def index
     @users = User.all
@@ -11,7 +12,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
   end
+
+  # private
+
+  # def find_user
+  #   @user = User.find(params[:id])
+  # end
+
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :picture)
+
+  # end
 
 end
