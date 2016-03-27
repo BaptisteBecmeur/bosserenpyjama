231401ec4092f0f8e1c3a5a62e0a5858d1564fed
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-27 18:31:49 +0200

    some views are done

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,26 +1,25 @@
 class FriendshipsController < ApplicationController
 
 def create
-  if current_user.friendships.where(friend_id: params[:friend_id]).any? || current_user.id == params[:friend_id]
-    flash[:notice] = "You already have added this user."
-    redirect_to users_path
-  else
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to current_user
-    else
-      flash[:error] = "Unable to add friend."
-      redirect_to current_user
-    end
-  end
+if current_user.friends.size >= 10 || current_user.friendships.where(friend_id: params[:friend_id]).any? || current_user.id == params[:friend_id]
+flash[:notice] = "You already have added this user."
+redirect_to users_path
+else
+@friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+if @friendship.save
+flash[:notice] = "Added friend."
+redirect_to current_user
+else
+flash[:error] = "Unable to add friend."
+redirect_to current_user
+end
+end
 end
 
 def destroy
-    @friendship = current_user.friendships.find(params[:id])
-    @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to current_user
-  end
+@friendship = current_user.friendships.find(params[:id])
+@friendship.destroy
+flash[:notice] = "Removed friendship."
+redirect_to current_user
 end
-
+end
