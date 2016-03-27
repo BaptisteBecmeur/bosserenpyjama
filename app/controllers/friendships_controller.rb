d9a477be5c2b0aaaaa8608f2e5049e973e856ef8
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-03-27 16:51:02 +0200

    some controllers added

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
+
 def create
-  if current_user.friendships.where(friend_id: params[:friend_id]).any?
+  if current_user.friendships.where(friend_id: params[:friend_id]).any? || current_user.id == params[:friend_id]
     flash[:notice] = "You already have added this user."
     redirect_to users_path
   else
