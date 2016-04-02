99d77f79f2120ef9fa8ba65f9539d6ec729c69e9
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-04-02 12:22:56 +0200

    reindentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,34 +2,33 @@ class CommentsController < ApplicationController
 
 before_action :set_post
 
-def create
-  @comment = @post.comments.build(comment_params)
-  @comment.user = current_user
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
 
-  if @comment.save
-    flash[:notice] = "You commented the hell out of that post!"
-    redirect_to :back
-  else
-    flash[:notice] = "Check the comment form, something went horribly wrong."
-    render root_path
+    if @comment.save
+      flash[:notice] = "You commented the hell out of that post!"
+      redirect_to :back
+    else
+      flash[:notice] = "Check the comment form, something went horribly wrong."
+      render root_path
+    end
   end
-end
 
-def destroy
-  @comment = @post.comments.find(params[:id])
-  @comment.destroy
-  flash[:notice] = "Comment deleted :("
-  redirect_to :back
-end
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment deleted :("
+    redirect_to :back
+  end
 
 private
 
-def set_post
-  @post = Post.find(params[:post_id])
-end
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
-def comment_params
-  params.require(:comment).permit(:content, :post_id, :user_id)
-end
-
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
 end
